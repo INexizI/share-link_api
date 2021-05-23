b111e8f04c639d02ac23b56d21ae05fdd8fc1c6c
INexizI <39536903+INexizI@users.noreply.github.com>
2021-05-23 18:18:33 +0300

    upd db

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  # root to: redirect('/api/v1/links')
+
   namespace :api do
     namespace :v1 do
       resources :links
-      
+
       post 'authenticate', to: 'authentication#create'
     end
   end
