0ccd601cd89b88e6917cbd7c03d71707cbd6536d
INexizI <39536903+INexizI@users.noreply.github.com>
2021-06-09 11:16:33 +0300

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
-    resource '*', headers: :any, methods: [:get, :post, :head]
+    # origins 'http://localhost:3000'
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post]
   end
 end
